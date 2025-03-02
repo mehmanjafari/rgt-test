7ff30863e528923dbfc0aa5019f36b9edac7d90a
mehmanjafari <165276993+mehmanjafari@users.noreply.github.com>
2025-03-02 18:09:15 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,35 @@ button.addEventListener("click", () => {
 });
 
 //Калькулятор дохода========================================================================================================================================================
+function calculate() {
+  const selectNode = document.querySelector(".js-form-income__sel");
+  const rangeNode = document.querySelector(".js-form-income__range");
+  const labelRangeDayClockNode = document.querySelector(
+    ".js-form-income__label"
+  );
+  const daySumNode = document.querySelector(".js-item-statistick__value-day");
+  const monthSumNode = document.querySelector(
+    ".js-item-statistick__value-month"
+  );
+
+  let selectValue = selectNode.value;
+
+  function updateResults() {
+    const rangeValue = rangeNode.value;
+    const resultDay = rangeValue * selectValue;
+    const resultMonth = rangeValue * selectValue * 30;
+
+    labelRangeDayClockNode.textContent = rangeValue;
+    daySumNode.textContent = `${resultDay}000`;
+    monthSumNode.textContent = `${resultMonth}000`;
+  }
+
+  selectNode.addEventListener("change", (e) => {
+    selectValue = e.target.value;
+    updateResults();
+  });
+
+  rangeNode.addEventListener("input", updateResults);
+}
+
+calculate();
